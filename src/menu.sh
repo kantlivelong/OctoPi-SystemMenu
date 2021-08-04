d424b76d4859cc954fbeb07e4e37a1bf358b4eb6
Shawn Bruce <kantlivelong@gmail.com>
2021-08-04 18:55:05 -0400

    Fix dmesg parser again

--- src/menu.sh
+++ src/menu.sh
@@ -374,7 +374,7 @@ do_tools_detect_printer_usb_serial() {
   echo "Please remove your printers USB cable then press ENTER to continue..."
   read
 
-  DMESG_LAST_MSG=$(dmesg | grep -E '^\[\s*[0-9\.]+\]' | tail -n 1 | awk ' { print $1 } ')
+  DMESG_LAST_MSG=$(dmesg | grep -oE '^\[\s*[0-9\.]+\]'| tail -n 1)
   udevadm monitor -p > $TMPFILE_UDEV_MONITOR &
   UDEV_MONITOR_PID=$!
 
